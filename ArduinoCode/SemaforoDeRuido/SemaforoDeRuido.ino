41582059eea7daa40c70a93017580574059d712a
Adrián Gil <adriangn@users.noreply.github.com>
2025-04-14 19:13:18 +0200

    Mejoras en el Semáforo de Ruido: Documentación, efectos visuales, optimización y funcionamiento VU meter

--- ArduinoCode/SemaforoDeRuido/SemaforoDeRuido.ino
+++ ArduinoCode/SemaforoDeRuido/SemaforoDeRuido.ino
@@ -1,212 +1,349 @@
-//Credits:  https://blog.yavilevich.com/2016/08/arduino-sound-level-meter-and-spectrum-analyzer/
+/*
+ * SemaforoDeRuido
+ * 
+ * Descripción:
+ * Este proyecto implementa un medidor de nivel de ruido ambiental que visualiza los
+ * decibelios detectados a través de una tira de LEDs NeoPixel, funcionando como un
+ * semáforo de ruido. Los niveles bajos se muestran en verde, los medios en amarillo
+ * y los altos en rojo, proporcionando una referencia visual inmediata del nivel sonoro.
+ * 
+ * Componentes:
+ * - Arduino (compatible con AVR)
+ * - Micrófono conectado al pin A0
+ * - Tira de LEDs NeoPixel conectada al pin 6
+ * 
+ * Funcionalidad:
+ * - Muestrea el sonido ambiente y calcula el nivel en decibelios (dB)
+ * - Visualiza el nivel de sonido en la tira de LEDs con escala de colores
+ * - Rango calibrado aproximadamente de 40 a 90 dB
+ * 
+ * Niveles de ruido y su significado:
+ * - Verde (Nivel bajo): Por debajo de 70 dB
+ *   Seguro para la mayoría de las personas en exposición prolongada.
+ * - Amarillo (Nivel moderado): Entre 70 dB y 85 dB
+ *   Puede ser molesto y con exposición continua podría empezar a ser perjudicial para la salud.
+ * - Rojo (Nivel alto): Por encima de 85 dB
+ *   Riesgo de daño auditivo con exposiciones prolongadas. Se recomienda usar protección auditiva en estos niveles.
+ */
 
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
-  #include <avr/power.h>
+#include <avr/power.h>
 #endif
 
-
 // pins
-#define MicPin A0 // used with analogRead mode only
-#define LedPin 6 
+#define MicPin A0
+#define LedPin 6
 
 // consts
 #define AmpMax (1024/2)
-#define MicSamples (1024*2) // Three of these time-weightings have been internationally standardised, 'S' (1 s) originally called Slow, 'F' (125 ms) originally called Fast and 'I' (35 ms) originally called Impulse.
+#define MicSamples (512)  // Reducido de 1024*2 a 512 para mayor velocidad
+#define USE_3V3 true       // Usar referencia de 3.3V
+#define ADC_FLOW true      // Usar ADC en modo flujo
+#define DbCalibration 94   // Calibración en decibelios
+#define FILTER_FACTOR 0.3  // Factor para filtrado de valores (aumentado para respuesta más rápida)
 
-// modes
-#define Use3.3 // use 3.3 voltage. the 5v voltage from usb is not regulated. this is much more stable.
-//#define ADCReClock // switch to higher clock, not needed if we are ok with freq between 0 and 4Khz.
-#define ADCFlow // read data from adc with free-run (not interupt). much better data, dc low. hardcoded for A0.
+// Niveles de ruido en decibelios
+#define DB_NIVEL_BAJO 70    // Verde
+#define DB_NIVEL_MEDIO 85   // Amarillo
+// Por encima de DB_NIVEL_MEDIO es rojo
 
-#define FreqLog // use log scale for FHT frequencies
-#ifdef FreqLog
-#define FreqOutData fht_log_out
-#define FreqGainFactorBits 0
-#else
-#define FreqOutData fht_lin_out8
-#define FreqGainFactorBits 3
-#endif
-#define FreqSerialBinary
-
-#define VolumeGainFactorBits 0
-
-#define DbCalibration 94
-
-#define awgsize 5
-
-// macros
-// http://yaab-arduino.blogspot.co.il/2015/02/fast-sampling-from-analog-input.html
-#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+// Definiciones para acceso directo al ADC
 #define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
 
+// Variables globales
 int prevdb = 0;
-
 const int maxScale = 12;
-const int redZone = 8;
-const int orangeZone = 5;
-
 Adafruit_NeoPixel pixels(maxScale, LedPin, NEO_GRB + NEO_KHZ800);
 
-const int sampleWindow = 50; // Sample window width in mS (50 mS = 20Hz)
-unsigned int sample;
+// Variable global para el efecto de flash
+unsigned long lastFlashTime = 0;
+boolean flashState = false;
+const int flashInterval = 150; // Intervalo de flash en ms
 
+// Variables para controlar el tiempo de actualización
+unsigned long lastUpdateTime = 0;
+const int updateInterval = 20;  // Tiempo mínimo entre actualizaciones en ms
 
-void setup() 
-{
-	//pinMode(MicPin, INPUT); // relevant for digital pins. not relevant for analog. however, don't put into digital OUTPUT mode if going to read analog values.
+// Variables globales adicionales para el peak holding
+float peakLevel = 0;          // Nivel de pico actual
+float peakDecayRate = 0.08;   // Velocidad de caída del pico (ajustado a un valor intermedio)
+unsigned long lastPeakTime = 0;  // Para controlar la caída del pico
+const int peakDecayInterval = 75; // Intervalo en ms para actualizar la caída del pico (ajustado)
 
-#ifdef ADCFlow
-	// set the adc to free running mode
-	// register explanation: http://maxembedded.com/2011/06/the-adc-of-the-avr/
-	// 5 => div 32. sample rate 38.4
-	// 7 => switch to divider=128, default 9.6khz sampling
-	ADCSRA = 0xe0+7; // "ADC Enable", "ADC Start Conversion", "ADC Auto Trigger Enable" and divider.
-	ADMUX = 0x0; // use adc0 (hardcoded, doesn't use MicPin). Use ARef pin for analog reference (same as analogReference(EXTERNAL)).
-#ifndef Use3.3
-	ADMUX |= 0x40; // Use Vcc for analog reference.
-#endif
-	DIDR0 = 0x01; // turn off the digital input for adc0
-#else
-#ifdef Use3.3
-	analogReference(EXTERNAL); // 3.3V to AREF
-#endif
-#endif
+void setup() {
+  // Configuración del ADC
+  if (ADC_FLOW) {
+    ADCSRA = 0xe0+7;
+    ADMUX = 0x0;
+    if (!USE_3V3) {
+      ADMUX |= 0x40;
+    }
+    DIDR0 = 0x01;
+  } else if (USE_3V3) {
+    analogReference(EXTERNAL);
+  }
 
-#ifdef ADCReClock // change ADC freq divider. default is div 128 9.6khz (bits 111)
-	// http://yaab-arduino.blogspot.co.il/2015/02/fast-sampling-from-analog-input.html
-	// 1 0 0 = mode 4 = divider 16 = 76.8khz
-	//sbi(ADCSRA, ADPS2);
-	//cbi(ADCSRA, ADPS1);
-	//cbi(ADCSRA, ADPS0);
-	// 1 0 1 = mode 5 = divider 32 = 38.4Khz
-	sbi(ADCSRA, ADPS2);
-	cbi(ADCSRA, ADPS1);
-	sbi(ADCSRA, ADPS0);
-#endif
-
-
-   Serial.begin(9600);
-    pixels.begin();
-
-animation(10);
-
-
-
+  Serial.begin(9600);
+  pixels.begin();
+  animation(3);
 }
 
-int averagegdb(int prev, int next){
-  return (next * (1/awgsize) + prev * (1-(1/awgsize)))/2 ; 
+// Función para filtrar los valores de dB
+int filtrarDB(int prev, int next) {
+  return prev * (1-FILTER_FACTOR) + next * FILTER_FACTOR;
 }
 
-
-
-  int previousValue = 0;
-void loop() 
-{
-
-
-
-   int db = MeasureVolume();
-
-   int avgdb = averagegdb(prevdb, db);
-    prevdb = db;
-	Serial.print(db);
-	Serial.print(",");
-  Serial.println(avgdb);
-
-
-   // map 1v p-p level to the max scale of the display
-   int displayPeak = map(db, 40, 90, 0, maxScale);
-
-
+// Función para crear color HSV (facilita crear arcoíris)
+uint32_t ColorHSV(uint16_t h, uint8_t s, uint8_t v) {
+  uint8_t r, g, b;
   
-   // draw the new sample
-   for (int i = 0; i <= maxScale; i++)
-   {
-      if ( !((i == displayPeak | i == previousValue) | ( displayPeak > previousValue  & i < displayPeak & i > previousValue ) | ( displayPeak < previousValue  & i > displayPeak & i < previousValue )   ) )
-      {
-        pixels.setPixelColor(i, pixels.Color(0, 2, 2));
-      }
-      else if (i >= orangeZone & i < redZone) // draw in green
-      {
-        pixels.setPixelColor(i, pixels.Color(125, 125, 0));
-        if(i != displayPeak) pixels.setPixelColor(i, pixels.Color(3, 3, 0));
-      }
-      else if (i < redZone) // draw in green
-      {
-        pixels.setPixelColor(i, pixels.Color(0, 250, 0));
-        if(i != displayPeak) pixels.setPixelColor(i, pixels.Color(0, 8, 0));
-      }
-      else // Red Alert!  Red Alert!
-      {
-        pixels.setPixelColor(i, pixels.Color(250, 0, 0));
-        if(i != displayPeak) pixels.setPixelColor(i, pixels.Color(8, 0, 0));
-      }
-   }
-   pixels.show();;  // write the changes we just made to the strip
-   previousValue = displayPeak;
+  // Convertir HSV a RGB
+  uint8_t region, remainder, p, q, t;
+  
+  if (s == 0) {
+    r = v; g = v; b = v;
+    return pixels.Color(r, g, b);
+  }
+  
+  region = h / 43;
+  remainder = (h - (region * 43)) * 6; 
+  
+  p = (v * (255 - s)) >> 8;
+  q = (v * (255 - ((s * remainder) >> 8))) >> 8;
+  t = (v * (255 - ((s * (255 - remainder)) >> 8))) >> 8;
+  
+  switch (region) {
+    case 0: r = v; g = t; b = p; break;
+    case 1: r = q; g = v; b = p; break;
+    case 2: r = p; g = v; b = t; break;
+    case 3: r = p; g = q; b = v; break;
+    case 4: r = t; g = p; b = v; break;
+    default: r = v; g = p; b = q; break;
+  }
+  
+  return pixels.Color(r, g, b);
 }
 
+void loop() {
+  // Control de frecuencia de actualización
+  unsigned long currentTime = millis();
+  if (currentTime - lastUpdateTime < updateInterval) {
+    return;  // Aún no es tiempo de actualizar
+  }
+  lastUpdateTime = currentTime;
+  
+  // Medir el nivel de ruido en dB
+  int db = MeasureVolume();
+  int avgdb = filtrarDB(prevdb, db);
+  prevdb = avgdb;  // Guardar el valor filtrado
+  
+  // Calcular el valor de pico con parte decimal para mayor resolución
+  float rangoDb, minDb, maxDb;
+  float displayPeakFloat;
+  
+  if (db < DB_NIVEL_BAJO) {
+    // Nivel bajo (verde)
+    minDb = 40;
+    maxDb = DB_NIVEL_BAJO;
+    rangoDb = maxDb - minDb;
+    displayPeakFloat = maxScale/3.0 * (db - minDb) / rangoDb;
+  } else if (db < DB_NIVEL_MEDIO) {
+    // Nivel medio (amarillo)
+    minDb = DB_NIVEL_BAJO;
+    maxDb = DB_NIVEL_MEDIO;
+    rangoDb = maxDb - minDb;
+    displayPeakFloat = maxScale/3.0 + (maxScale/3.0) * (db - minDb) / rangoDb;
+  } else {
+    // Nivel alto (rojo)
+    minDb = DB_NIVEL_MEDIO;
+    maxDb = 90;
+    rangoDb = maxDb - minDb;
+    displayPeakFloat = 2*maxScale/3.0 + (maxScale/3.0) * (db - minDb) / rangoDb;
+  }
+  
+  // Limitar el valor a los límites de la escala
+  if (displayPeakFloat < 0) displayPeakFloat = 0;
+  if (displayPeakFloat > maxScale) displayPeakFloat = maxScale;
+  
+  // Actualizar el nivel de pico
+  if (displayPeakFloat > peakLevel) {
+    // Si el nivel actual es mayor que el pico, actualizar inmediatamente
+    peakLevel = displayPeakFloat;
+    lastPeakTime = currentTime;
+  } else if (currentTime - lastPeakTime > peakDecayInterval) {
+    // Decrementar el nivel de pico lentamente
+    peakLevel -= peakDecayRate;
+    if (peakLevel < displayPeakFloat) {
+      peakLevel = displayPeakFloat;
+    }
+    lastPeakTime = currentTime;
+  }
+  
+  // Imprimir valores en serial: db,holdingbar
+  Serial.print(db);
+  Serial.print(",");
+  
+  // Convertir el nivel de pico a decibelios para la salida serial
+  float peakDb;
+  if (peakLevel < maxScale/3.0) {
+    // Zona verde
+    peakDb = 40 + (DB_NIVEL_BAJO - 40) * peakLevel / (maxScale/3.0);
+  } else if (peakLevel < 2*maxScale/3.0) {
+    // Zona amarilla
+    peakDb = DB_NIVEL_BAJO + (DB_NIVEL_MEDIO - DB_NIVEL_BAJO) * (peakLevel - maxScale/3.0) / (maxScale/3.0);
+  } else {
+    // Zona roja
+    peakDb = DB_NIVEL_MEDIO + (90 - DB_NIVEL_MEDIO) * (peakLevel - 2*maxScale/3.0) / (maxScale/3.0);
+  }
+  
+  Serial.println(peakDb);
+  
+  // Obtener el índice del LED de pico y su decimal
+  int peakLedIndex = (int)peakLevel;
+  float peakDecimal = peakLevel - peakLedIndex;
+  
+  // Asegurar que el índice de peak está dentro del rango válido
+  if (peakLedIndex >= maxScale) {
+    peakLedIndex = maxScale - 1;
+    peakDecimal = 1.0;
+  }
 
+  // Verificar si estamos en nivel de alarma (rojo alto)
+  boolean isAlarmLevel = (peakDb >= DB_NIVEL_MEDIO);
+  
+  // Control del efecto de flash para nivel de alarma
+  if (isAlarmLevel) {
+    // Actualizar estado del flash
+    if (currentTime - lastFlashTime > flashInterval) {
+      lastFlashTime = currentTime;
+      flashState = !flashState;
+    }
+  } else {
+    // Fuera de nivel de alarma, reset del estado de flash
+    flashState = false;
+  }
 
+  // Determinar el color base según el nivel actual
+  uint32_t colorBase;
+  if (peakDb >= DB_NIVEL_MEDIO) {
+    // Nivel rojo
+    colorBase = pixels.Color(255, 0, 0);
+  } else if (peakDb >= DB_NIVEL_BAJO) {
+    // Nivel amarillo - reducida intensidad para equilibrar con rojo y verde
+    colorBase = pixels.Color(180, 180, 0);
+  } else {
+    // Nivel verde
+    colorBase = pixels.Color(0, 255, 0);
+  }
 
+  // Actualizar todos los LEDs - Solo basado en el nivel de pico
+  for (int i = 0; i < maxScale; i++) {
+    uint32_t color;
+    
+    if (i < peakLedIndex) {
+      // LEDs completamente activos
+      if (isAlarmLevel && !flashState) {
+        // Efecto de flash: cuando está en OFF mostramos un rojo más tenue
+        color = pixels.Color(80, 0, 0); // Rojo más tenue durante el flash
+      } else {
+        color = colorBase;
+      }
+    } 
+    else if (i == peakLedIndex) {
+      // LED de transición con brillo proporcional a la parte decimal
+      if (peakDb >= DB_NIVEL_MEDIO) {
+        // Versión variable del rojo
+        if (isAlarmLevel && !flashState) {
+          // En flash OFF, usar un rojo más tenue
+          color = pixels.Color(peakDecimal * 80, 0, 0);
+        } else {
+          color = pixels.Color(peakDecimal * 255, 0, 0);
+        }
+      } else if (peakDb >= DB_NIVEL_BAJO) {
+        // Versión variable del amarillo - reducida intensidad
+        color = pixels.Color(peakDecimal * 180, peakDecimal * 180, 0);
+      } else {
+        // Versión variable del verde
+        color = pixels.Color(0, peakDecimal * 255, 0);
+      }
+    }
+    else {
+      // LED inactivo - baja luminosidad
+      if (peakDb >= DB_NIVEL_MEDIO) {
+        // Versión tenue del rojo
+        color = pixels.Color(2, 0, 0);
+      } else if (peakDb >= DB_NIVEL_BAJO) {
+        // Versión tenue del amarillo - reducida intensidad
+        color = pixels.Color(1, 1, 0);
+      } else {
+        // Versión tenue del verde
+        color = pixels.Color(0, 1, 0);
+      }
+    }
+    
+    pixels.setPixelColor(i, color);
+  }
+  
+  pixels.show();
+}
 
+void animation(int iteraciones) {
+  int sine[] = {0,1,2,3,4,5,6,7,8,9,10,11};
+  
+  // Hacer efecto arcoíris
+  for (int j=0; j<iteraciones; j++) {
+    // Hacer dos vueltas completas de colores por cada iteración
+    for (int colorStep = 0; colorStep < 360; colorStep += 10) {  // Pasos más pequeños (10 en lugar de 15)
+      
+      for (int i=0; i<12; i++) {
+        // Crear un efecto ondulante con desplazamiento de color
+        int hue = (colorStep + i*30) % 360;
+        uint32_t color = ColorHSV(hue, 255, 255);
+        
+        pixels.setPixelColor(sine[i], color);
+      }
+      
+      pixels.show();
+      delay(50);  // Delay más largo (50ms en lugar de 20ms)
+    }
+  }
+  
+  // Limpieza al final de la animación
+  for (int i=0; i<maxScale; i++) {
+    pixels.setPixelColor(i, 0);
+  }
+  pixels.show();
+}
 
-void animation(int iteraciones){
-  // These are the pixels in order of animation-- 36 pixels in total:
-int sine[] = {
-   0,1,2,3,4,5,6,7,8,9,10,11 };
-uint32_t color = pixels.Color(250, 14, 14); 
-for (int j=0; j<10; j++){
-for(int i=0; i<12; i++) {
-    pixels.setPixelColor(sine[i], color);             // Draw 'head' pixel
-    pixels.setPixelColor(sine[(i + 12 - 2) % 12], 0); // Erase 'tail'
-    pixels.show();
-    delay(40);
-  }}}
+float MeasureVolume() {
+  long soundVolAvg = 0, soundVolMax = 0, soundVolRMS = 0;
+  
+  // Muestrear el micrófono con un método optimizado
+  for (int i = 0; i < MicSamples; i++) {
+    int k;
+    
+    // Leer ADC según el modo configurado
+    if (ADC_FLOW) {
+      while (!(ADCSRA & _BV(ADIF)));  // Esperar a que esté listo el ADC
+      sbi(ADCSRA, ADIF);              // Resetear el flag
+      k = ADC;                        // Leer el valor directamente (más rápido)
+    } else {
+      k = analogRead(MicPin);
+    }
+    
+    // Procesar muestra con cálculos optimizados
+    int amp = abs(k - AmpMax);
+    soundVolRMS += ((long)amp*amp);  // Solo calculamos RMS para decibelios
+  }
 
+  // Calcular RMS directamente
+  soundVolRMS /= MicSamples;
+  float soundVolRMSflt = sqrt(soundVolRMS);
 
-  // calculate volume level of the signal and print to serial and LCD
-float MeasureVolume()
-{
-	long soundVolAvg = 0, soundVolMax = 0, soundVolRMS = 0, t0 = millis();
-	//cli();  // UDRE interrupt slows this way down on arduino1.0
-	for (int i = 0; i < MicSamples; i++)
-	{
-#ifdef ADCFlow
-		while (!(ADCSRA & /*0x10*/_BV(ADIF))); // wait for adc to be ready (ADIF)
-		sbi(ADCSRA, ADIF); // restart adc
-		byte m = ADCL; // fetch adc data
-		byte j = ADCH;
-		int k = ((int)j << 8) | m; // form into an int
-#else
-		int k = analogRead(MicPin);
-#endif
-		int amp = abs(k - AmpMax);
-		amp <<= VolumeGainFactorBits;
-		soundVolMax = max(soundVolMax, amp);
-		soundVolAvg += amp;
-		soundVolRMS += ((long)amp*amp);
-	}
-	soundVolAvg /= MicSamples;
-	soundVolRMS /= MicSamples;
-	float soundVolRMSflt = sqrt(soundVolRMS);
-	//sei();
-
-	float dB = 20.0*log10(soundVolRMSflt/AmpMax)+DbCalibration;
-
-	// convert from 0 to 100
-	soundVolAvg = 100 * soundVolAvg / AmpMax; 
-	soundVolMax = 100 * soundVolMax / AmpMax; 
-	soundVolRMSflt = 100 * soundVolRMSflt / AmpMax;
-	soundVolRMS = 10 * soundVolRMSflt / 7; // RMS to estimate peak (RMS is 0.7 of the peak in sin)
-
-	// print
-	//Serial.print("Time: " + String(millis() - t0));
-	//Serial.print(" Amp: Max: " + String(soundVolMax));
-	//Serial.print("% Avg: " + String(soundVolAvg));
-
+  // Calcular decibelios
+  float dB = 20.0*log10(soundVolRMSflt/AmpMax) + DbCalibration;
+  
   return dB;
-
 }
